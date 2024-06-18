a121497860d4c5390a4dd141845d3797c03ca091
Matthieu <matthieu.colin@epita.fr>
2024-06-18 12:23:56 +0200

    feat: poc with face_recognition
    Signed-off-by: Matthieu <matthieu.colin@epita.fr>

--- src/main2.py
+++ src/main2.py
@@ -12,7 +12,8 @@ async def main():
     await mpc.start()
 
     # Récupération de l'image du runner local
-    img_path = '/home/matthieu/srs/crypto/14-secure-biometric-auth-SMC/data/Aaron_Peirsol/Aaron_Peirsol_0003.jpg' # input("Path of the image: ")
+    img_path = '/home/matthieu/srs/crypto/14-secure-biometric-auth-SMC/data/Aaron_Peirsol/Aaron_Peirsol_0001.jpg' # input("Path of the image: ")
+
     # Extraction des vecteurs propres de l'image
     image = face_recognition.load_image_file(img_path)
     embedding = face_recognition.face_encodings(image)[0]
