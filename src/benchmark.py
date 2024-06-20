3daf727c370dff6a23cb844cf3066e0abe676fa7
Jules Diaz <jules.diaz@epita.fr>
2024-06-20 23:21:57 +0200

    feat: benchmark
    Signed-off-by: Jules Diaz <jules.diaz@epita.fr>

--- src/benchmark.py
+++ src/benchmark.py
@@ -26,6 +26,7 @@ def list_all_files(directory, extension):
     res.sort()
     return res
 
+
 def is_same_person(file1, file2):
     """
 
@@ -38,6 +39,7 @@ def is_same_person(file1, file2):
     dossier1 = Path(file1).resolve().parent
     dossier2 = Path(file2).resolve().parent
     return dossier1 == dossier2
+
 
 async def test_images(image1, image2, threshold):
     """
@@ -55,46 +57,49 @@ async def test_images(image1, image2, threshold):
     return result <= threshold
 
 
-async def process_benchmark(res):
+async def process_benchmark_iterator(res):
+    await mpc.start()
+
+    for _ in range(len(res)):
+        for tested in res:
+            await compute(tested)
+
+    await mpc.shutdown()
 
 
+async def process_benchmark_reference(res):
     # This is the parent process
+    await mpc.start()
 
-    for image1 in res:
-        for image2 in res:
-            print("Testing image {} with image {}".format(image1, image2))
-            expected = is_same_person(image1, image2)
+    for reference in res:
+        for tested in res:
+            print("Testing image {} with image {}".format(reference, tested))
+            expected = is_same_person(reference, tested)
             # result = await test_images(image1, image2, threshold)
-            await mpc.start()
-            result = await compute(image2)
-            await mpc.shutdown()
-            if expected and result <= threshold:
+            result = await compute(reference)
+
+            if result == -1:
+                print("Face not found")
+            elif expected and result <= threshold:
                 print("Failed, expected {} but got {}".format(expected, result))
             else:
                 print("Passed")
 
+    await mpc.shutdown()
 
-    """
-    if pid != 0:
-        await mpc.shutdown()
-    """
 
 # python3.10 benchmark.py --data /home/matthieu/srs/crypto/14-secure-biometric-auth-SMC/data/ -M2 -I1
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--data", type=str, required=True, help="path to the data folder")
+    parser.add_argument("--iterator", action="store_true", help="set if the program is an iterator that will provide all images")
     args = parser.parse_args()
 
     res = list_all_files(args.data, '.jpg')
     threshold = 0.4
 
-    pid = os.fork()
-
-    if pid == 0:
-        # This is the child process
-        for image_child1 in res:
-            for image_child2 in res:
-                subprocess.run(['python3.10', 'main.py', '-M2', '-I0', '--image', image_child2], stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
+    if args.iterator:
+        mpc.run(process_benchmark_iterator(res))
     else:
-        mpc.run(process_benchmark(res))
+        subprocess.Popen(['python3.10', 'benchmark.py', '--iterator', '--data', args.data, '-M2', '-I0'], stderr=subprocess.PIPE)
+        mpc.run(process_benchmark_reference(res))
--- src/main.py
+++ src/main.py
@@ -8,32 +8,39 @@ import argparse
 secflt = mpc.SecFlt()
 secfpx = mpc.SecFxp(32)
 
+
 async def compute(img_path):
     # Récupération de l'image du runner local
     # img_path = '/home/matthieu/srs/crypto/14-secure-biometric-auth-SMC/data/Aaron_Peirsol/Aaron_Peirsol_0003.jpg' # input("Path of the image: ")
     # Extraction des vecteurs propres de l'image
     image = face_recognition.load_image_file(img_path)
-    embedding = face_recognition.face_encodings(image)[0]
+
+    try:
+        embedding = face_recognition.face_encodings(image)[0]
+    except:
+        return -1
+
     # embedding = DeepFace.represent(img_path=img_path)[0]['embedding']
-    embedding =  secfpx.array(embedding)
+    embedding = secfpx.array(embedding)
 
     # Récupération de l'image de la seconde partie
     user, server = mpc.input(embedding)
 
-    print('Computing the distance')
+    # print('Computing the distance')
     distance = np.subtract(user, server)
-    print('Computing the euclidian distance')
-    print('Multiply')
+    # print('Computing the euclidian distance')
+    # print('Multiply')
     euclidian = np.multiply(distance, distance)
-    print('Sum')
+    # print('Sum')
     euclidian = np.sum(euclidian)
 
-    print('Printing the result')
+    # print('Printing the result')
     euclidian = await mpc.output(euclidian)
     # print('Sqrt')
     euclidian = np.sqrt(euclidian)
-    print('Result', euclidian)
+    # print('Result', euclidian)
     return euclidian
+
 
 async def main(img_path):
     # Initialiser le runtime MPyC
