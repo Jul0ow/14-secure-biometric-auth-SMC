26e9fdf72b49ef662d25b7e069cf81bae61b7157
Matthieu <matthieu.colin@epita.fr>
2024-06-20 15:50:51 +0200

    feat: add function to test if images represent the same person
    Signed-off-by: Matthieu <matthieu.colin@epita.fr>

--- src/benchmark.py
+++ src/benchmark.py
@@ -1,6 +1,8 @@
 import argparse
 import os
 import subprocess
+from pathlib import Path
+
 from main import process
 
 
@@ -21,15 +23,48 @@ def list_all_files(directory, extension):
     res.sort()
     return res
 
+def is_same_person(file1, file2):
+    """
+
+    Test if two files have the same person. Which means in our case that the two files belongs to the same directory
+
+    :param file1: the first file representing the first person
+    :param file2: the second file representing the second person
+    """
+
+    dossier1 = Path(file1).resolve().parent
+    dossier2 = Path(file2).resolve().parent
+    return dossier1 == dossier2
+
+def test_images(image1, image2, threshold):
+    """
+
+    Test whether two images represent the same person using MPyC.
+
+    :param image1: the path of the first image
+    :param image2: the path of the second image
+    :return: True if images represent the same person. False otherwise.
+    """
+
+    subprocess.Popen(['python3.10', 'main.py', '-M2', '-I0', '--image', image1], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    result = process(image2)
+
+    return result <= threshold
+
+
+
 def main(data_dir):
     res = list_all_files(data_dir, '.jpg')
     # for s in res:
         # print(s)
-    print("RIRI")
-    result = subprocess.Popen(['python3.10', 'main.py', '-M2', '-I0', '--image', res[0]], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    print("RORO", result)
-    print("Returned", process(res[0]))
 
+    expected = is_same_person(res[0], res[1])
+    result = test_images(res[0], res[1], 0.4)
+
+    if expected != result:
+        print("Failed")
+    else:
+        print("Passed")
 
 # python3.10 benchmark.py --data /home/matthieu/srs/crypto/14-secure-biometric-auth-SMC/data/ -I0
 if __name__ == '__main__':
